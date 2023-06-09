654e51bafe3b19a75c34c0e4f1a73906144d8e3a
Yoon <dltkddbs0714@naver.com>
2023-06-09 10:11:55 +0900

    2023_06_09 경로수정

--- js/my-page-membership.js
+++
@@ -1,6 +0,0 @@
-const couponbtn = document.querySelector(".my-page-coupon-btn");
-
-couponbtn.addEventListener("click",()=>{
-    window.open("my-page-coupon.html","_blank",
-    "width=850,height=600,top=100,left=100");
-})
