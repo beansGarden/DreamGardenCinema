ff25c148a25338c45830efce18ac48f8b47faa0c
yuminkim <bfyu@naver.com>
2023-06-09 17:49:57 +0900

    230609 DB

--- db/sample/Yumin(notice,qna)_0609.sql
+++ db/sample/Yumin(notice,qna)_0609.sql
@@ -80,3 +80,82 @@ VALUES(SEQ_USER_NO.NEXTVAL, 'user10', 'password', '유저십', '01012341010', 'u
       , DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, '021010-4');
 
 COMMIT;
+
+/*qna 등록*/
+
+INSERT INTO "QNA"
+VALUES(SEQ_QNA_NO.NEXTVAL, '영화 상영 시작하고 20분 지난 후에도 입장이 가능한가요?', '당일에 조금 늦을 거 같은데 20분이 지나도 입장 시켜주나요??'
+		,'2023-05-01', DEFAULT, DEFAULT,'P',14);
+
+INSERT INTO "QNA"
+VALUES(SEQ_QNA_NO.NEXTVAL, '영화관 안에서 햄버거 먹을 수 있나요?', '외부음식 가능하다고 써있는데 햄버거는 가능할까요?'
+		,'2023-05-10', DEFAULT, DEFAULT,'P',14);	
+	
+INSERT INTO "QNA"
+VALUES(SEQ_QNA_NO.NEXTVAL, '영화관 안에서 지갑을 잃어버렸어요.', '12:30 1관에서 상영한 범죄도시3을 보고 나왔는데 지갑이 없어진 걸 몇 시간 뒤에 알았습니다.
+가서 찾을 수 있을까요?'
+		,'2023-05-12', DEFAULT, DEFAULT,'P',13);	
+	
+INSERT INTO "QNA"
+VALUES(SEQ_QNA_NO.NEXTVAL, '10분 전에 예매하고 바로 취소 가면 환불 가능한가요?', '영화 시작 10분전에 예매하고 바로 취소하면 환불 가능할까요?'
+		,'2023-05-12', DEFAULT, DEFAULT,'B',12);	
+
+INSERT INTO "QNA"
+VALUES(SEQ_QNA_NO.NEXTVAL, '로그인 문제', '안녕하세요. 로그인이 잘 안 돼서 연락 드립니다. 비밀번호도 맞는데 자꾸 틀리다고 하네요.
+확인 좀 해주세요.','2023-05-13', DEFAULT, DEFAULT,'H',11);		
+
+INSERT INTO "QNA"
+VALUES(SEQ_QNA_NO.NEXTVAL, '회원탈퇴 시켜주세요', '안녕하세요. 회원탈퇴하는데 오류가 자꾸 나요. 탈퇴 시켜주세요.','2023-05-15', DEFAULT, DEFAULT,'A',11);	
+
+SELECT * FROM "QNA";
+
+COMMIT;
+
+/*qna 댓글*/
+
+/*문의 댓글 테이블 컬럼 추가*/
+ALTER TABLE "QNA _COMMENT" ADD QNA_COMMENT VARCHAR2(3000) NOT NULL;
+
+COMMENT ON COLUMN "QNA _COMMENT"."QNA_COMMENT" IS '문의댓글';
+
+COMMIT;
+
+SELECT * FROM "QNA _COMMENT";
+
+INSERT INTO "QNA _COMMENT"
+VALUES(SEQ_QNA_NO.NEXTVAL,'2023-05-01',DEFAULT,14,1,' 안녕하세요. 드림가든시네마 입니다. 20분 지나도 입장가능합니다.');	
+
+INSERT INTO "QNA _COMMENT"
+VALUES(SEQ_QNA_NO.NEXTVAL,'2023-05-10',DEFAULT,14,2,' 안녕하세요. 드림가든시네마 입니다. 외부음식 가능합니다.');	
+
+INSERT INTO "QNA _COMMENT"
+VALUES(SEQ_QNA_NO.NEXTVAL,'2023-05-12',DEFAULT,13,3,'안녕하세요. 드림가든시네마 입니다. 상영 후 청소할 때 분실물은 창구에 맡겨두오니
+
+창구에서 확인 하시기 바랍니다. 그래도 찾지 못 하시는 경우에는 직원에게 문의 바랍니다.');
+
+INSERT INTO "QNA _COMMENT"
+VALUES(SEQ_QNA_NO.NEXTVAL,'2023-05-12',DEFAULT,12,4,'영화 환불은 10분전까지만 가능합니다. 남은 시간이 10분 이하가 되면 환불은 불가능하오니
+이 점 유의하시기 바랍니다.');	
+
+INSERT INTO "QNA _COMMENT"
+VALUES(SEQ_QNA_NO.NEXTVAL,'2023-05-13',DEFAULT,11,5,'핸드폰 번호 또는 연락처 남겨주시면 직원이 처리하여 연락드리겠습니다.');	
+
+INSERT INTO "QNA _COMMENT"
+VALUES(SEQ_QNA_NO.NEXTVAL,'2023-05-15',DEFAULT,11,6,'핸드폰 번호 또는 연락처 남겨주시면 직원이 처리하여 연락드리겠습니다.');	
+
+COMMIT;
+
+/*FAQ*/
+
+INSERT INTO "FAQ"
+VALUES(SEQ_FAQ_NO.NEXTVAL,'인터넷 예매시 부분환불/교환이 가능한가요?','인터넷 예매시에는 예매매수의 전체환불 및 교환만 가능합니다.',DEFAULT,'B');	
+
+INSERT INTO "FAQ"
+VALUES(SEQ_FAQ_NO.NEXTVAL,'아이디를 바꾸고 싶은데 수정할 수 있나요?','회원가입시 입력한 아이디는 변경이 불가하며,
+탈퇴 후 재가입(신규가입)을 통해 새로운 아이디로 지정할 수 있습니다.
+
+단, 탈퇴 시 재가입은 30일이후부터 가능합니다.',DEFAULT,'A');	
+
+SELECT * FROM "FAQ";
+
+COMMIT;
