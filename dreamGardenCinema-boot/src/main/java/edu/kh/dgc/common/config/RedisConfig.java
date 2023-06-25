885fe2c2daf8d1ab2d7c19298cdecde1ac5a442f
lys5034 <lys5034@nate.com>
2023-06-26 08:56:25 +0900

    오류수정

--- dreamGardenCinema-boot/src/main/java/edu/kh/dgc/common/config/RedisConfig.java
+++ dreamGardenCinema-boot/src/main/java/edu/kh/dgc/common/config/RedisConfig.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
-@PropertySource("classpath:/config2.properties")
+@PropertySource("classpath:/config.properties")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
