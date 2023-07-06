5f95f4d1f09f3c56d1ebe67b1777925680eb61c7
chanhee2635 <cksgml0156@naver.com>
2023-07-06 17:24:14 +0900

    20230706 찬희

---
+++ dreamGardenCinema-boot/src/main/java/edu/kh/dgc/common/filter/AdminFilter.java
@@ -0,0 +1,41 @@
+package edu.kh.dgc.common.filter;
+
+import java.io.IOException;
+
+import edu.kh.dgc.user.model.dto.User;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter(filterName="AdminFilter",
+urlPatterns = {"/admin", "/adminMain", "/admin/*", "/ticketAmount", "/adminUser", "/adminUserOut", "/adminUser/*", "/getUserSearchList", "/adminUserListAjax", "/adminUserInListAjax"
+		,"/adminUserOutListAjax", "/adminCinemaManage", "/adminCinemaTimeSelect", "/adminCinemaDeleteTime", "/adminCinemaDelete", "/adminCinemaRegister", "/adminCinemaRegisterinsert", "/adminCinemaListAjax", "/adminCinemaInsert", "/adminNotice"
+		, "/adminNoticeDeleted", "/adminNoticeRead/*", "/getNoticeSearchList", "/adminNoticeWrite", "/adminNoticeWriteInsert", "/adminNoticeUpdate/*", "/adminNoticeRead/*", "/adminNotice/*", "/adminNoticeListAjax", "/adminNoticeInListAjax", "/adminNoticeOutListAjax"
+		, "/adminQna", "/adminQnaRead/*", "/adminQnaWrite", "/adminQnaWriteInsert", "/adminQnaAnswer/*"})
+
+public class AdminFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		HttpSession session = req.getSession();
+		
+		User user = (User) session.getAttribute("loginUser");
+		
+		if(user == null || !user.getUserRole().equals("A")) {
+			resp.sendRedirect("/");
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+}
